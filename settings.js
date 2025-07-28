605c5277981acb09ff0d96a6827ad89cf9cc7ea1
zikky0001-droid <zikkystar0001@gmail.com>
2025-07-28 13:37:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "DIVINE MD",
+  botOwner: 'DIVINE MD', // Your name
+  ownerNumber: '2348054483474', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
